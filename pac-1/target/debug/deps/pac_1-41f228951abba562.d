15250f48ddb62449b5e249cfe873670c8f4be64c
Luis Antonio Momm Duarte <luismomm@gmail.com>
2023-03-04 19:14:42 -0300

    fist commit

--- pac-1/target/debug/deps/pac_1-41f228951abba562.d
+++
@@ -1,5 +0,0 @@
-/home/luismomm/Documents/Graduação/23-1/PAC/catolica-pac-oitavo/pac-1/target/debug/deps/pac_1-41f228951abba562: src/main.rs
-
-/home/luismomm/Documents/Graduação/23-1/PAC/catolica-pac-oitavo/pac-1/target/debug/deps/pac_1-41f228951abba562.d: src/main.rs
-
-src/main.rs:
--- pac-1/target/debug/libpac_1.d
+++
@@ -1,1 +0,0 @@
-/home/luismomm/Documents/Graduação/23-1/PAC/catolica-pac-oitavo/pac-1/target/debug/libpac_1.rlib: /home/luismomm/Documents/Graduação/23-1/PAC/catolica-pac-oitavo/pac-1/src/lib.rs
